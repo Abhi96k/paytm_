86b96b86b19ca7d703aa5e65c9aa92cc984486b2
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-20 09:22:15 +0530

    refactor HDFC webhook handler to include validation and secret check for request authenticity

--- paytm-project-starter-monorepo/apps/bank_webhook_handler/src/index.ts
+++ paytm-project-starter-monorepo/apps/bank_webhook_handler/src/index.ts
@@ -7,11 +7,10 @@ app.use(express.json());
 app.post("/hdfcWebhook", async (req, res) => {
   //TODO: Add zod validation here?
   //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
-  const paymentInformation: {
-    token: string;
-    userId: string;
-    amount: string;
-  } = {
+  
+  //check if this request accetually came from HDFC bank,use webhooh secret here
+  
+  const paymentInformation = {
     token: req.body.token,
     userId: req.body.user_identifier,
     amount: req.body.amount,
@@ -25,7 +24,6 @@ app.post("/hdfcWebhook", async (req, res) => {
         },
         data: {
           amount: {
-            // You can also get this from your DB
             increment: Number(paymentInformation.amount),
           },
         },
